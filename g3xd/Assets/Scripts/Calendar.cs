b608fbb175c9895752d07f8ab4cd70625d7f793f
PaRoVoZ0 <parowozovich6@yandex.ru>
2023-08-03 19:22:25 +0300

    Test Commit
    Тест коммита

---
+++ g3xd/Assets/Scripts/Calendar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calendar : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("TI SLAVA MARLOW!?!?!?!");
+    }
+}
