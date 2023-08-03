e0f28d4fb43528659a4757ff0b5fb10be848522f
PaRoVoZ0 <parowozovich6@yandex.ru>
2023-08-03 20:38:19 +0300

    Update Calendar.cs

--- g3xd/Assets/Scripts/Calendar.cs
+++ g3xd/Assets/Scripts/Calendar.cs
@@ -1,11 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Calendar : MonoBehaviour
 {
+    [SerializeField] private Text _calendarText;
+    [SerializeField] private Button _nextTurnBtn;
+
+    private DateTime _startDate = new DateTime(2023,9,1);
+    private DateTime _currentDate;
+
+    [SerializeField] private double _interval = 1;
+
     private void Start()
     {
-        Debug.Log("TI SLAVA MARLOW!?!?!?!");
+        _currentDate = _startDate;
+        _calendarText.text = _currentDate.ToString("dd-MM-yyyy");
+        //Debug.Log(currentDate);
+    }
+
+    public void DateChange()
+    {
+        _currentDate = _currentDate.AddDays(_interval);
+        _calendarText.text = _currentDate.ToString("dd-MM-yyyy");
+        Debug.Log(_currentDate);
     }
 }
